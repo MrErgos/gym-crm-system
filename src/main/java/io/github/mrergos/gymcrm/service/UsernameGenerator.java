1eb8f39e337bd43c877bae84428d38d6a74e85e9
George <mrergos.babin@gmail.com>
2026-06-22 03:31:05 +0500

    feature: create UsernameGenerator service generate username base on uniqueness in database

---
+++ src/main/java/io/github/mrergos/gymcrm/service/UsernameGenerator.java
@@ -0,0 +1,36 @@
+package io.github.mrergos.gymcrm.service;
+
+import io.github.mrergos.gymcrm.dao.TraineeDao;
+import io.github.mrergos.gymcrm.dao.TrainerDao;
+import io.github.mrergos.gymcrm.util.UserUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsernameGenerator {
+    private static final Logger log = LoggerFactory.getLogger(UsernameGenerator.class);
+
+    private TraineeDao traineeDao;
+    private TrainerDao trainerDao;
+
+    @Autowired
+    public void setTraineeDao(TraineeDao traineeDao) {
+        this.traineeDao = traineeDao;
+    }
+
+    @Autowired
+    public void setTrainerDao(TrainerDao trainerDao) {
+        this.trainerDao = trainerDao;
+    }
+
+    public String generate(String firstName, String lastName) {
+        String username = UserUtils.generateUniqueUsername(firstName,
+                lastName,
+                usernameCandidate -> traineeDao.existsByUsername(usernameCandidate) || trainerDao.existsByUsername(usernameCandidate));
+
+        log.debug("Generated username '{}' for {} {}", username, firstName, lastName);
+        return username;
+    }
+}
