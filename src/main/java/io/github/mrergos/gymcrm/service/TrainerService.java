108f05b1811c95249493bdd581b5804a432071e1
George <mrergos.babin@gmail.com>
2026-06-22 03:37:27 +0500

    feature: implement service classes for Trainee, Trainer and Training Services check that some required fields are valid for example that first name and last name are not blank because username based on them

---
+++ src/main/java/io/github/mrergos/gymcrm/service/TrainerService.java
@@ -0,0 +1,17 @@
+package io.github.mrergos.gymcrm.service;
+
+import io.github.mrergos.gymcrm.entity.Trainer;
+import io.github.mrergos.gymcrm.entity.TrainingType;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TrainerService {
+    Trainer createTrainerProfile(String firstName, String lastName, TrainingType specialization);
+
+    Trainer updateTrainerProfile(Trainer trainer);
+
+    Optional<Trainer> getTrainerProfile(Long id);
+
+    List<Trainer> getAllTrainer();
+}
