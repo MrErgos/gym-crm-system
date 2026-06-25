c029d252ea63f8c66b7c037f13641d7d49c55d98
George <mrergos.babin@gmail.com>
2026-06-25 18:24:28 +0500

    feature(in-memory storage): now use @PostConstruct and @PreDestroy instead of BeanPostProcessor Additionally add ability to save the storage to the file

--- src/main/java/io/github/mrergos/gymcrm/storage/StorageInitializer.java
+++ src/main/java/io/github/mrergos/gymcrm/storage/StorageInitializer.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.mrergos.gymcrm.entity.Trainee;
 import io.github.mrergos.gymcrm.entity.Trainer;
 import io.github.mrergos.gymcrm.entity.Training;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +15,10 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Map;
 
 @Component
@@ -59,6 +63,7 @@ public class StorageInitializer {
         this.objectMapper = objectMapper;
     }
 
+    @PostConstruct
     public void loadStorage() {
         if (filePath == null || filePath.isBlank()) {
             log.warn("Filepath is blank. Storage will be empty.");
@@ -79,10 +84,56 @@ public class StorageInitializer {
                 log.warn("Filepath is pointing to directory: {}. Storage will be empty.", filePath);
                 return;
             }
+
+            if (resource.contentLength() == 0) {
+                log.warn("Storage is empty: {}. Storage will be empty.", filePath);
+                return;
+            }
+
             StorageDTO data = objectMapper.readValue(is, StorageDTO.class);
             populateStorage(data);
         } catch (IOException e) {
             log.error("Failed to read storage file: {}", filePath, e);
+        }
+    }
+
+    @PreDestroy
+    public void saveStorage() {
+        if (filePath == null || filePath.isBlank()) {
+            log.warn("Filepath is blank. Storage will not be saved.");
+            return;
+        }
+        log.info("Saving storage to file: {}", filePath);
+
+        StorageDTO data = new StorageDTO();
+        data.setTrainees(new ArrayList<>(traineeStorage.values()));
+        data.setTrainers(new ArrayList<>(trainerStorage.values()));
+        data.setTrainings(new ArrayList<>(trainingStorage.values()));
+
+        Resource resource = resourceLoader.getResource(filePath);
+
+        try {
+            File file = resource.getFile();
+
+            File parentDir = file.getParentFile();
+            if (parentDir != null && !parentDir.exists()) {
+                if (!parentDir.mkdirs()) {
+                    log.error("Failed to create directories: {}", parentDir.getAbsolutePath());
+                    return;
+                }
+                log.debug("Created directories: {}", parentDir.getAbsolutePath());
+            }
+
+            if (!file.exists() && !file.createNewFile()) {
+                log.error("Failed to create storage file: {}", file.getAbsolutePath());
+                return;
+            }
+
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, data);
+            log.info("Storage saved. Trainee: {}, trainers: {}, trainings: {}",
+                    data.getTrainees().size(), data.getTrainers().size(), data.getTrainings().size());
+        } catch (IOException e) {
+            log.error("Failed to save storage to file: {}", filePath, e);
         }
     }
 
