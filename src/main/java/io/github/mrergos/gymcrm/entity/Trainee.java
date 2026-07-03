1b8697c962ff8a0805b0a3ec5295b0637c45fff4
George <mrergos.babin@gmail.com>
2026-07-03 23:39:49 +0500

    feature(entities): adjust to use in Hibernate -add annotations, field like id -move id from Trainee and Trainer class to parent User

--- src/main/java/io/github/mrergos/gymcrm/entity/Trainee.java
+++ src/main/java/io/github/mrergos/gymcrm/entity/Trainee.java
@@ -1,32 +1,43 @@
 package io.github.mrergos.gymcrm.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+@Table(name = "trainees")
+@PrimaryKeyJoinColumn(name = "user_id")
 @Getter
 @Setter
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 public class Trainee extends User {
+
+    @Column(name = "date_of_birth")
     private LocalDate dateOfBirth;
+
     private String address;
-    private Long userId;
+
+    @OneToMany(mappedBy = "trainee", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Training> trainings = new ArrayList<>();
 
     public Trainee(String firstName, String lastName, String username, String password,
                    boolean isActive,
                    LocalDate dateOfBirth, String address, Long userId) {
-        super(firstName, lastName, username, password, isActive);
+        super(userId, firstName, lastName, username, password, isActive);
         this.dateOfBirth = dateOfBirth;
         this.address = address;
-        this.userId = userId;
     }
 
     public Trainee(Trainee other) {
         super(other);
         this.dateOfBirth = other.dateOfBirth;
         this.address = other.address;
-        this.userId = other.userId;
     }
 }
--- src/main/java/io/github/mrergos/gymcrm/entity/Trainer.java
+++ src/main/java/io/github/mrergos/gymcrm/entity/Trainer.java
@@ -1,26 +1,39 @@
 package io.github.mrergos.gymcrm.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "trainers")
+@PrimaryKeyJoinColumn(name = "user_id")
 @Getter
 @Setter
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 public class Trainer extends User {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "specialization_id", nullable = false)
+    @EqualsAndHashCode.Exclude
     private TrainingType specialization;
-    private Long userId;
+
+    @OneToMany(mappedBy = "trainer", fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Training> trainings = new ArrayList<>();
+
 
     public Trainer(String firstName, String lastName, String username, String password,
                    boolean isActive, TrainingType specialization, Long userId) {
-        super(firstName, lastName, username, password, isActive);
+        super(userId, firstName, lastName, username, password, isActive);
         this.specialization = specialization;
-        this.userId = userId;
     }
 
     public Trainer(Trainer other) {
         super(other);
         this.specialization = other.specialization != null ? new TrainingType(other.specialization) : null;
-        this.userId = other.userId;
     }
 }
