d9b39c95387b1f7852dd07efc3c13be6f45fb2c3
George <mrergos.babin@gmail.com>
2026-07-26 06:02:27 +0500

    feature: delete swagger-ui resourceHandler because now Spring Boot does it

--- src/main/java/io/github/mrergos/gymcrm/config/WebConfig.java
+++ src/main/java/io/github/mrergos/gymcrm/config/WebConfig.java
@@ -13,12 +13,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/");
-    }
-
     @Bean
     public ObjectMapper jacksonObjectMapper() {
         ObjectMapper mapper = new ObjectMapper();
